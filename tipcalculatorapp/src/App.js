be9772931635e1e643de82400902b16931472296
Weslley Braga <wesbragagt@gmail.com>
2019-06-22 13:52:07 -0500

    dependecies and material ui kit

---
+++ tipcalculatorapp/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Button from '@material-ui/core/Button';
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to tip calculator</h1>
+                <Button>Testing</Button>
+            </div>
+        );
+    }
+}
+
+export default App;
